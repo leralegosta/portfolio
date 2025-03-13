0bd78db3d27f99a53d6986c0aefdbf27fa8be3b0
Lera <vl47623@uga.edu>
2025-03-13 14:14:07 -0400

    stuff

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,10 +7,10 @@ function About() {
     <div className="min-h-screen bg-first text-emerald-900 flex flex-col items-center justify-center px-8 sm:px-20 pt-20">
       <NavBar />
 
-      <main className="w-full max-w-lg bg-white p-6 rounded-2xl shadow-lg text-gray-900 mt-12">
+      <main className="w-full max-w-lg bg-white p-6 rounded-2xl shadow-lg text-emerald-900 mt-12">
         <h1 className="text-2xl font-bold text-center mb-4">Contact Me</h1>
         <p className="text-center mb-6">
-          Feel free to reach out with any questions or feedback!
+          placeholder txt
         </p>
 
         {/* Contact Form */}
@@ -20,7 +20,7 @@ function About() {
             <input
               type="text"
               placeholder="Enter your name"
-              className="mt-1 p-2 rounded-md border border-gray-300 focus:border-rose-500 focus:ring-1 focus:ring-rose-500 outline-none"
+              className="mt-1 p-2 rounded-md border border-gray-300 focus:border-rose-500 focus:ring-1 focus:ring-fourth outline-none"
             />
           </label>
 
@@ -29,7 +29,7 @@ function About() {
             <input
               type="email"
               placeholder="Enter your email"
-              className="mt-1 p-2 rounded-md border border-gray-300 focus:border-rose-500 focus:ring-1 focus:ring-rose-500 outline-none"
+              className="mt-1 p-2 rounded-md border border-gray-300 focus:border-rose-500 focus:ring-1 focus:ring-fourth outline-none"
             />
           </label>
 
@@ -38,13 +38,13 @@ function About() {
             <textarea
               placeholder="Write your message..."
               rows="4"
-              className="mt-1 p-2 rounded-md border border-gray-300 focus:border-rose-500 focus:ring-1 focus:ring-rose-500 outline-none resize-none"
+              className="mt-1 p-2 rounded-md border border-gray-300 focus:border-rose-500 focus:ring-1 focus:ring-fourth outline-none resize-none"
             />
           </label>
 
           <button 
             type="submit"
-            className="w-full bg-rose-500 text-white py-2 rounded-md font-semibold transition hover:bg-rose-600 active:scale-95"
+            className="w-full bg-fourth text-white py-2 rounded-md font-semibold transition hover:bg-first active:scale-95"
           >
             Send Message
           </button>
